682208a65e70361197bab0fb2310d6435afa06f1
sushilAadi <131242719+sushilAadi@users.noreply.github.com>
2026-02-24 20:47:39 +0530

    Comment out duration badge motion div
    Commented out the duration badge motion div for future reference.

--- src/components/ui/twitter-testimonial-cards.jsx
+++ src/components/ui/twitter-testimonial-cards.jsx
@@ -195,7 +195,7 @@ function DetailPanel({ company }) {
           </motion.div>
 
           {/* Duration Badge */}
-          <motion.div
+          {/* <motion.div
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.15 }}
@@ -205,7 +205,7 @@ function DetailPanel({ company }) {
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
             </svg>
             <span className="font-medium text-primary text-xs">{company.duration}</span>
-          </motion.div>
+          </motion.div> */}
 
           {/* Description */}
           <motion.p
